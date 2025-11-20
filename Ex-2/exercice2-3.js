bfa95330a29c443d8be74ba6b8c5fdb02438fd72
MattWare <schlitmatthias@gmail.com>
2025-11-20 16:14:56 +0100

    js

---
+++ Ex-2/exercice2-3.js
@@ -0,0 +1,14 @@
+/* let i = 1;
+
+while (i <= 100) {
+    if (i % 2 === 0) {
+        console.log(i);
+        alert(i);
+    }
+    i++;
+} */
+
+for (let j = 0; j < 50; j++) {
+    console.log(j + 1);
+    alert(j + 1);
+}
