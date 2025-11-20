407ea6eae26a5dd8b60316d2bc79c553bf54f499
Mattware2912 <schlitmatthias@gmail.com>
2025-11-20 11:17:18 +0100

    js

--- exercice2-2.js
+++ exercice2-2.js
@@ -1,14 +1,15 @@
 let min = prompt("écris un nombre");
 let max = prompt("écris un nombre");
-let current = prompt("écris un nombre entre " + min +" et " + max + " :");
-
-if (min > max) {
-    alert("oups ! la valeur minimale est plus grande que la valeur maximale");
-} 
 
 if (current >= min && current <= max) {
         alert("le nombre est comprit entre min et max");
-    }
+}
 
+else {
+    let current = prompt("écris un nombre");
 
-
+if (min > max) {
+    console.log("oups ! la valeur minimale est plus grande que la valeur maximale");
+    alert("oups ! la valeur minimale est plus grande que la valeur maximale");
+} 
+}
